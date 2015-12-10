33ab5b99d2345b9e2366a26f5b4fdc4977aa9651
phenix007 <igor.pyanyx.97@mail.ru>
2015-12-10 21:05:00 +0200

    Обработчик кнопки "Расчитать"

--- calc_rent/calc_rent/Form1.cs
+++ calc_rent/calc_rent/Form1.cs
@@ -70,5 +70,75 @@ namespace calc_rent
                 reader.Close();
             }
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Width = 824;
+            this.Height = 392;
+            itog.Clear();
+            itog.AppendText("Вывоз мусора:" + 30 + '\n');
+            itog.AppendText("Обслуживание лифта:" + 30 + '\n');
+            itog.AppendText("Ремонт здания:" + 70 + '\n');
+            elect = Convert.ToSingle(electr.Text) * Convert.ToSingle(textBox5.Text);
+            itog.AppendText("За электричество:" + elect.ToString() + '\n');
+
+            if (!GasCB.Checked) gas = Convert.ToSingle(gast.Text);
+            else gas = Convert.ToSingle(gast.Text) * Convert.ToSingle(textBox4.Text);
+            itog.AppendText("За газ:" + gas.ToString() + '\n');
+
+            if (!HotW.Checked) hwater = Convert.ToSingle(hwatert.Text);
+            else hwater = Convert.ToSingle(hwatert.Text) * Convert.ToSingle(textBox3.Text);
+            itog.AppendText("За горячую воду:" + hwater.ToString() + '\n');
+
+            if (!ColW.Checked) cwater = Convert.ToSingle(cwatert.Text);
+            else cwater = Convert.ToSingle(cwatert.Text) * Convert.ToSingle(textBox2.Text);
+            itog.AppendText("За холодную воду:" + cwater.ToString() + '\n');
+
+            otopl = Convert.ToSingle(otoplt.Text) * Convert.ToSingle(pl.Value);
+            itog.AppendText("За отопление:" + otopl.ToString() + '\n');
+            comm = (otopl + hwater + cwater + gas + elect) * Convert.ToSingle(cH.Value);
+            itog.AppendText("Comm:" + comm.ToString() + '\n');
+            if ((lift.Checked) && (rubb.Checked) && (rep.Checked))
+            {
+                result = comm + lif + repair + musor;
+                itog.AppendText("Итого(лифт,мусор,газ):" + result.ToString() + '\n');
+            }
+            else if ((lift.Checked) && (rubb.Checked))
+            {
+                result = comm + lif + musor;
+                itog.AppendText("Итого(лифт,мусор):" + result.ToString() + '\n');
+            }
+            else if ((lift.Checked) && (rep.Checked))
+            {
+                result = comm + lif + repair;
+                itog.AppendText("Итого(лифт,ремонт):" + result.ToString() + '\n');
+            }
+            else if ((rubb.Checked) && (rep.Checked))
+            {
+                result = comm + repair + musor;
+                itog.AppendText("Итого(мусор,ремонт):" + result.ToString() + '\n');
+            }
+            else if (lift.Checked)
+            {
+                result = comm + lif;
+                itog.AppendText("Итого(лифт):" + result.ToString() + '\n');
+            }
+            else if (rubb.Checked)
+            {
+                result = comm + musor;
+                itog.AppendText("Итого(мусор):" + result.ToString() + '\n');
+            }
+            else if (rep.Checked)
+            {
+                result = comm + repair;
+                itog.AppendText("Итого(ремонт):" + result.ToString() + '\n');
+            }
+            else
+            {
+                result = comm;
+                itog.AppendText("Итого():" + result.ToString() + '\n');
+
+            }
+        }
+
     }
 }
